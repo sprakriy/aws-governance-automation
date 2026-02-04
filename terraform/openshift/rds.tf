9a50509fe42be5e470e03b35dd2c5183cbd24147
sprakriy <sprakriya@yahoo.com>
2026-02-04 18:37:12 -0500

    corrected syntax errors

--- terraform/openshift/rds.tf
+++ terraform/openshift/rds.tf
@@ -32,5 +32,6 @@ resource "aws_db_instance" "oracle_db" {
   skip_final_snapshot  = true
   
   # Network - This assumes you have a subnet group from your previous AWS work
-  db_subnet_group_name = aws_security_group.oracle_sg.id 
+  db_subnet_group_name = aws_db_subnet_group.default.name
+  vpc_security_group_ids = [aws_security_group.oracle_sg.id]
 }
